18932a7d9c8c0c33cdda0fd06a638abc55b42865
prefer2 <sunho6200@gmail.com>
2022-05-19 00:06:45 +0900

    feat: login page 구현

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./pages/Login/";
+import Register from "./pages/Register";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path={"/login"} element={<LoginPage />}></Route>
+        <Route path={"/register"} element={<Register />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
