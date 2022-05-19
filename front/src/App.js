9c0081b13db98066227f4caa3c688b1d49fbb8ac
prefer2 <sunho6200@gmail.com>
2022-05-19 23:54:03 +0900

    feat: main page 구현

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Main from "./pages/Main";
 import LoginPage from "./pages/Login/";
 import Register from "./pages/Register";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
+        <Route path={"/"} element={<Main />}></Route>
         <Route path={"/login"} element={<LoginPage />}></Route>
         <Route path={"/register"} element={<Register />}></Route>
       </Routes>
---
+++ front/src/pages/Main/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const LeftWrapper = styled.div`
+  padding: 10px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  width: 350px;
+`;
+
+const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export { LeftWrapper, FlexWrapper };
