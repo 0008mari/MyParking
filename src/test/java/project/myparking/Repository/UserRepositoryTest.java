730114794790bbb01718e2d026cc1b77718bbd8d
daeunchung <jul12230103@gmail.com>
2022-05-24 09:54:14 +0900

    User Test code

---
+++ src/test/java/project/myparking/Repository/UserRepositoryTest.java
@@ -0,0 +1,22 @@
+package project.myparking.Repository;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import project.myparking.domain.User;
+import project.myparking.repository.UserRepository;
+
+@SpringBootTest
+@Transactional
+public class UserRepositoryTest {
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Test
+//    public void testUser() {
+//        User user = new User("userA");
+//
+//    }
+}
