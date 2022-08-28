cb6a86b8dc6b2f583a1c6acc1dbdbce3d415c235
prefer2 <sunho6200@gmail.com>
2022-08-28 15:34:06 +0900

    feat: 초기 위치 현재 위치 중심으로 옮겨지도록 구현

--- front/src/pages/Map/index.jsx
+++ front/src/pages/Map/index.jsx
@@ -4,6 +4,20 @@ function Map() {
   useEffect(() => {
     let container = document.getElementById("map");
 
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude; // 위도
+        const lon = position.coords.longitude; // 경도
+
+        let options = {
+          center: new window.kakao.maps.LatLng(lat, lon),
+          level: 5,
+        };
+        let map = new window.kakao.maps.Map(container, options);
+      });
+
+      return;
+    }
     let options = {
       center: new window.kakao.maps.LatLng(
         37.365264512305174,
