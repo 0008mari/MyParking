bfb2f31a2c5553984fe7d37f6297145f6d756321
prefer2 <sunho6200@gmail.com>
2022-10-16 16:16:35 +0900

    chore: react-query 설정

--- front/src/index.js
+++ front/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
@@ -10,8 +11,12 @@ if (process.env.NODE_ENV === "development") {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
