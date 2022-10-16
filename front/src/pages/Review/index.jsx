6bc970eb998427a5f65c5bb34b6f9fe7339e67d1
prefer2 <sunho6200@gmail.com>
2022-10-16 16:19:30 +0900

    feat: review 작성 구현

--- front/src/pages/Review/index.jsx
+++ front/src/pages/Review/index.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 import { Button } from "antd";
+import { useMutation } from "@tanstack/react-query";
 
 import ReviewList from "./List";
 
 import { PageWrapper } from "./styled";
+import axios from "axios";
 
 function ReviewPage() {
   const [selectedReview, setSelectedReview] = useState({});
+
+  const { mutate } = useMutation(() => {
+    axios.post("/reviews/new", {
+      space: selectedReview[0],
+      parkingLevel: selectedReview[1],
+      price: selectedReview[2],
+      staff: selectedReview[3],
+      revisit: selectedReview[4],
+      score: selectedReview[5],
+    });
+  });
 
   const handleButtonClick = (e, index) => {
     setSelectedReview((prev) => ({ ...prev, [index]: e.target.value }));
@@ -30,7 +43,7 @@ function ReviewPage() {
       return;
     }
 
-    console.log(selectedReview);
+    mutate();
   };
 
   return (
